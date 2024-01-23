7fd8d19e8af2fa8a43f804aac65003a2be6fa016
jcarodas <jenro1092@gmail.com>
2024-01-22 19:50:46 -0500

    Added city and weather API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  let newTemperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(newTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "2f841tfa2a2f0faob14b1085393c7be0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function SearchSubmit(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = searchFormInput.value;
+
+  searchCity(searchFormInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", SearchSubmit);
+
+searchCity("Berlin");
