76f166049ab98afc242ebb28d2848648b2fcfdce
jcarodas <jenro1092@gmail.com>
2024-01-22 21:42:00 -0500

    Changed data javascript

--- src/index.js
+++ src/index.js
@@ -2,9 +2,40 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#weather-temperature");
   let newTemperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-city");
+  let weatherDescription = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#dateAndTime");
+  let currentDate = new Date(response.data.time * 1000);
+  console.log(response);
 
   cityElement.innerHTML = response.data.city;
+  weatherDescription.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  timeElement.innerHTML = formatDate(currentDate);
   temperatureElement.innerHTML = Math.round(newTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
