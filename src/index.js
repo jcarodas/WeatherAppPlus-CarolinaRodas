d12ffd13551d4481baf0c5dc189cc4a618258993
jcarodas <jenro1092@gmail.com>
2024-01-27 19:01:12 -0500

    Added JS template

--- src/index.js
+++ src/index.js
@@ -53,7 +53,25 @@ function SearchSubmit(event) {
   searchCity(searchFormInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = " ";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">${day}</div>
+                <div class="weather-forecast-icon"><img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png" width="40" /></div>
+                <div class="weather-forecast-temperatures"><span class="weather-forecast-temperature-max">18°</span> <span class="weather-forecast-temperature-min"> 12°</span></div> 
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", SearchSubmit);
 
 searchCity("Berlin");
+displayForecast();
